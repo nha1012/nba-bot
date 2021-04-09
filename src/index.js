4cd8dfe53656e41036394e4bcb1e24584047db9f
nguyen nha <nguyenphucnha111@gmail.com>
2021-04-09 11:17:40 +0700

    asd

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-const { getGames } = require('nba-stats-client');
-const moment = require('moment');
-
 module.exports = async function App(context) {
-  if (context.event.isText && moment(context.event.text, 'YYYY/M/D', true).isValid()) {
-    const timestamp = moment(context.event.text, 'YYYY/M/D');
-    const data = await getGames({
-      year: timestamp.format('YYYY'),
-      month: timestamp.format('M'),
-      day: timestamp.format('D'),
-    });
-
-    const games = data.sports_content.games.game;
-    const scores = games.map(({ visitor, home }) => {
-      const score = `${visitor.abbreviation} ${visitor.score} : ${home.abbreviation} ${home.score}`;
-      return score;
-    });
-
-    await context.sendText(`${timestamp.format('YYYY/MM/DD')}\n${scores.join('\n')}`);
+  console.log(context.event);
+  if (context.event.isText && context.event.message.text ==='Nhã ơiii') {
+    await context.sendText(`Sao`);
   }
 };
